2c351cee2747ff5aa5fee1b81863d696ad8261bf
ismqdb <djukic.djukic96@gmail.com>
2025-06-05 18:00:50 +0200

    create circle

--- includes/circle/circle.c
+++ includes/circle/circle.c
@@ -3,3 +3,23 @@
 #include "circle.h"
 
 /* ******************************************************************************** */
+
+struct circle createCircle(struct vec4 _pos, float _rad, int noOfTri){
+    assert(_rad > 0);
+    assert(noOfTri > 4);
+
+    struct circle circle;
+
+    circle.position = _pos;
+    circle.radius = _rad;
+    circle.noOfTriangles = noOfTri;
+    circle.arcLen = 360.0/noOfTri;
+
+    struct array pointArray;
+
+    
+
+    return circle;
+}
+
+/* ******************************************************************************** */
